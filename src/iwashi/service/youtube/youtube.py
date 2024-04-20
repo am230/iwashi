8c37714533a6bf2df19da9dbcf9c41b85968ea13
am230 <111672334+am230@users.noreply.github.com>
2024-04-20 20:40:03 +0900

    fix: youtube.com/<VANITY_ID>なurlに対応

--- src/iwashi/service/youtube/youtube.py
+++ src/iwashi/service/youtube/youtube.py
@@ -38,7 +38,7 @@ class Youtube(Service):
             )
         if type in ("channel", "user", "c"):
             return await self._channel_by_url(context, url)
-        return url
+        return uri.path.split("/")[1]
 
     async def _channel_by_video(self, context: Context, video_id: str) -> str | None:
         result = await self._channel_by_oembed(context, video_id)
