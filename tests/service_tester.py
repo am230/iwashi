2bc7ca2f004c9c2d04829e859dd26b36bb02d0dc
am230 <111672334+am230@users.noreply.github.com>
2024-04-20 22:29:51 +0900

    test: テストのエラーメッセージを分かりやすく

--- tests/service_tester.py
+++ tests/service_tester.py
@@ -24,25 +24,32 @@ async def _test_service(service: Service, correct_result: Result, *urls: str) ->
     session = aiohttp.ClientSession(headers=BASE_HEADERS)
     for url in urls:
         context = Context(session=session, visitor=visitor)
+        resolved_id = await service.resolve_id(context, url)
         assert (
-            await service.resolve_id(context, url) == correct_result.id
-        ), f"Failed to resolve id for {url}"
+            resolved_id == correct_result.id
+        ), f"ID mismatch for {url} ({resolved_id} != {correct_result.id})"
 
     # visit
     for url in urls:
         result = await service.visit_url(session, url)
         assert result, f"Failed to visit {url}"
-        assert result.url == correct_result.url, f"URL mismatch for {url}"
-        assert result.service == correct_result.service, f"Service mismatch for {url}"
-        assert result.name == correct_result.name, f"Name mismatch for {url}"
+        assert (
+            result.url == correct_result.url
+        ), f"URL mismatch for {url} ({result.url} != {correct_result.url})"
+        assert (
+            result.service == correct_result.service
+        ), f"Service mismatch for {url} ({result.service} != {correct_result.service})"
+        assert (
+            result.name == correct_result.name
+        ), f"Name mismatch for {url} ({result.name} != {correct_result.name})"
         assert (
             result.description == correct_result.description
-        ), f"Description mismatch for {url}"
+        ), f"Description mismatch for {url} ({result.description} != {correct_result.description})"
         assert (
             correct_result.profile_picture
             and result.profile_picture
             and (correct_result.profile_picture in result.profile_picture)
-        ), f"Profile picture mismatch for {url}"
+        ), f"Profile picture mismatch for {url} ({result.profile_picture} != {correct_result.profile_picture})"
         assert iterable_eq(
             result.links, correct_result.links
-        ), f"Links mismatch for {url}"
+        ), f"Links mismatch for {url} ({result.links} != {correct_result.links})"
