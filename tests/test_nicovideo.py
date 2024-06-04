ff2828209d486365f260cae627931181c29571e4
2.30am <111672334+am230@users.noreply.github.com>
2024-06-04 15:21:55 +0900

    test: test_nicovideo.pyを更新

--- tests/test_nicovideo.py
+++ tests/test_nicovideo.py
@@ -9,14 +9,14 @@ async def test_nicovideo():
     service = Nicovideo()
     correct = Result(
         service=service,
-        id="128134532",
-        url="https://www.nicovideo.jp/user/128134532",
-        name="ラベンダーP",
-        description="岡山県備前市日生町に住む2006年生まれの人 <br>好きなもの（鉄道、東方、艦これ、ボカロ、ボイロ、野球、中日、阪神、西武、オリックス、ロッテ、railwars、すくすく白沢）",
-        profile_picture="https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/12813/128134532.jpg?1710066983",
+        id="2008672",
+        url="https://www.nicovideo.jp/user/2008672",
+        name="生パン",
+        description='東方アレンジや絵など。UPした曲は、すべて自サイトにmp3を置いてあります！生パン庫： <a href="http://www.namapann.com/" target="_blank" rel="noopener nofollow">http://www.namapann.com/</a><br>',
+        profile_picture="https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/200/2008672.jpg?1263354782",
         links={
-            "https://www.youtube.com/channel/UC4jehnRY1GBPBpg-Np4WFNg",
-            "https://twitter.com/lavenderp2018",
+            "https://www.youtube.com/channel/UCVYIzyZuQvyzuQFzihLnA8Q",
+            "https://twitter.com/namapann",
         },
     )
-    await _test_service(service, correct, "https://www.nicovideo.jp/user/128134532")
+    await _test_service(service, correct, "https://www.nicovideo.jp/user/2008672")
